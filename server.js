e440555534b4aeb394630886c27b5f0599a68ffc
zachatrocity <zachatrocity@gmail.com>
2016-02-19 14:31:12 -0600

    added perform opponent move

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ cloak.configure({
       user.message('refreshRoomResponse', user.room.getMembers(true));
     },
 
-    turnDone: function(targetId, user){
+    turnDone: function(from,to, user){
       // If it's currently the turn of the user and they say they're done, advance the turn
       if (user.team === user.room.turn) {
         user.room.turn = (user.room.turn === 'muon') ? 'antimuon' : 'muon';
@@ -86,10 +86,10 @@ cloak.configure({
       var otherPlayer = _.reject(user.room.members, function(member) {
         return member.id === user.id;
       });
-      otherPlayer[0].message('placedTarget', [targetId, user.room.lastMove]);
+      otherPlayer[0].message('performOpponentMove', [from,to, user.room.lastMove]);
 
       user.room.messageMembers('turn', user.room.turn);
-    
+
     }
   },
 
